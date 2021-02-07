7da8da8849eb1d359d46dc7950aefe80372c48c0
Daniel Gomes <danielgds@id.uf.br>
2021-02-07 11:19:09 -0300

    feat: model job are created

---
+++ src/models/Job.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const db = require('../database/connection');
+
+const Job = db.define('Jobs', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    salary: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    company: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    new: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    }
+
+})
+
+module.exports = Job;
